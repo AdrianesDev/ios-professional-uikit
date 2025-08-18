1d7a28c4cc31adaa03a95badf015f9cb0f734759
Adrian Castañeda Rodriguez <ing.adrian.castaneda@gmail.com>
2025-08-18 14:18:08 -0700

    ios app

---
+++ Bankey/Bankey/Login/LoginView.swift
@@ -0,0 +1,36 @@
+//
+//  LoginView.swift
+//  Bankey
+//
+//  Created by Adrian Castañeda on 15/08/25.
+//
+import Foundation
+import UIKit
+
+class LoginView: UIView {
+    let usernameTextField = UITextField()
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        style()
+        layout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override var intrinsicContentSize: CGSize {
+        return CGSize(width: 200, height: 200)
+    }
+}
+
+extension LoginView {
+    func style() {
+        translatesAutoresizingMaskIntoConstraints = false
+        backgroundColor = .orange
+        usernameTextField.translatesAutoresizingMaskIntoConstraints = false
+    }
+    func layout() {
+        
+    }
+}
---
+++ Bankey/Bankey/Login/ViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController.swift
+//  Bankey
+//
+//  Created by Adrian Castañeda on 15/08/25.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    let loginView = LoginView()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        style()
+        layout()
+    }
+
+
+}
+
+
+
+extension LoginViewController {
+    private func style() {
+        loginView.translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    private func layout() {
+        view.addSubview(loginView)
+        NSLayoutConstraint.activate(
+[
+            loginView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            loginView.leadingAnchor
+                .constraint(
+                    equalToSystemSpacingAfter: view.leadingAnchor,
+                    multiplier: 1
+                ),
+            view.trailingAnchor
+                .constraint(
+                    equalToSystemSpacingAfter: loginView.trailingAnchor,
+                    multiplier: 1
+                )
+        ]
+)
+    }
+}
+
